e1c6864937409bd5cfe9e5e94dfc4ed23259ce2d
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-12 23:36:48 +0000

    Added a random number generator for use in shortURL creation, allso added body-parser package to make responses legible

--- express_server.js
+++ express_server.js
@@ -1,13 +1,30 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+function generateRandomString() {
+  let randomStr = "";
+  const possibleChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOQRSTUVWXYZ0123456789";
+
+  for (let i = 0; i < 6; i ++) {
+    randomStr += possibleChar.charAt(Math.floor(Math.random() * possibleChar.length));
+  }
+  return randomStr;
+}
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
