04abbef4f44924dffd8512fb176f54d8bb301ebe
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-14 18:54:14 +0000

    Added functionality where new user can be added to database with unique ID, their email+password

--- express_server.js
+++ express_server.js
@@ -24,8 +24,29 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 app.get("/register", (req, res) => {
-  res.render("urls_index");
+  res.render("urls_register");
+})
+
+app.post("/register", (req, res) => {
+  const { email, password} = req.body;
+  usersId = generateRandomString();
+  users[usersId] = {id: usersId, email: email, password: password};
 })
 
 app.post("/login", (req, res) => {
