f091e175d5f63a7e275af85e1c4bd12e60a2b6ff
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-14 00:41:20 +0000

    Added cookies, updated partial headers to ask for username if none is present, else welcomes back user by name

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
 
 function generateRandomString() {
   let randomStr = "";
@@ -13,13 +14,20 @@ function generateRandomString() {
   return randomStr;
 }
 
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/login", (req, res) => {
+res.cookie("username", req.body.username);
+res.redirect(`/urls`);
+})
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
@@ -34,21 +42,24 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new")
+  let templateVars = {
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 })
 
 app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect('/urls');
+  res.redirect('/urls', templateVars);
 })
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 })
 
